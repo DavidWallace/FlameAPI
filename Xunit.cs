24350315973995a299175a49656e9dd531277510
DavidWallace <dav.wallace8@gmail.com>
2019-04-29 17:49:12 +0100

    Final Adjustments
    Testing and Final Adjustments

---
+++ Xunit.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using FlameAPI.Model.Entities;
+using FlameAPI.Services.Repositories;
+using Xunit;
+namespace FlameAPI
+{
+    public class Xunit
+    {
+    
+        private ProjectRepository _projectRepository;
+        [Fact]
+
+        public void returnsProjectList()
+        {
+            var y = _projectRepository.listAllProjects();
+
+
+            Assert.IsType<List<Projects>>(y);
+        }
+
+        public void returnsProject()
+        {
+            var y = _projectRepository.getProject(5);
+
+
+            Assert.IsType<Projects>(y);
+        }
+
+        [Fact]
+        public void returnsNull()
+        {
+            var y = _projectRepository.getProject(-1);
+
+
+            Assert.NotNull(y);
+        }
+
+        [Fact]
+        public void returnsUnique()
+        {
+            var y = _projectRepository.getProject(5);
+            var x = _projectRepository.getProject(6);
+
+
+            Assert.NotSame(y, x);
+        }
+
+    }
+}
